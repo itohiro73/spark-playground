d2ad6e2f60fa1de8c4f25e6792e9199bd88b8ccc
Hiroshi Ito <itohiro73@gmail.com>
2015-08-27 23:35:22 +0900

    Create temperature processor

---
+++ scala/src/main/scala/jp/itohiro/spark/playground/TemperatureProcess.scala
@@ -0,0 +1,41 @@
+package jp.itohiro.spark.playground
+
+import java.sql.Timestamp
+
+import org.apache.spark.{SparkContext, SparkConf}
+
+object TemperatureProcess {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("Temperature Process App")
+    val sc = new SparkContext(conf)
+
+    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+
+    import sqlContext.implicits._
+
+    val statsFile = sc.textFile("/home/spark/input/stats")
+
+    val stats = statsFile.map(s => s.split(",")).map(s => Stats(s(0), s(1), Timestamp.valueOf(s(2)), Integer.parseInt(s(3))))
+
+    val df = stats.toDF().groupBy("host").avg("temp")
+
+    df.show()
+  }
+
+  case class Stats(host:String, ip:String, datetime:Timestamp, temp: Integer)  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("Temperature Process App")
+    val sc = new SparkContext(conf)
+
+    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+
+    import sqlContext.implicits._
+
+    val statsFile = sc.textFile("/home/spark/input/stats")
+
+    val stats = statsFile.map(s => s.split(",")).map(s => Stats(s(0), s(1), Timestamp.valueOf(s(2)), Integer.parseInt(s(3))))
+
+    val df = stats.toDF().groupBy("host").avg("temp")
+
+    df.show()
+  }
+}
